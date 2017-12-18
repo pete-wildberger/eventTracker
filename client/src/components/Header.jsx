f3960583bcad45776ecf8cf6293a801692a1a6da
Peter Wildberger <pete.wildberger@gmail.com>
2017-12-18 12:46:51 -0600

    spelling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = props => {
         <div className="pushdown">
           <h1 className="center">West Bank Events</h1>
           <h2 className="center">Cedar | Palmer's | Nomad | Acadia</h2>
-          <p className="center">Live Music in the Cedar/Riverside neightborhood of Minneapolis</p>
+          <p className="center">Live Music in the Cedar/Riverside neighborhood of Minneapolis</p>
         </div>
       </div>
     </div>
